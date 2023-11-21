d673ba3911ea4a5d600e269fc9eef9c5f010e659
G00405014@atu.ie <G00405014@atu.ie>
2023-11-21 16:04:31 +0000

    Added Rest controller

--- src/main/java/PersonController.java
+++ src/main/java/PersonController.java
@@ -1,2 +1,36 @@
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+@RestController
 public class PersonController {
+    private final PersonController personController;
+    public PersonController(PersonController PersonController) {
+        this.personController = personController;
+
+    }
+    @GetMapping("/{employeeId}")
+    public ResponseEntity<?> getPerson(@PathVariable String employeeId){
+        if(employeeId.length() > 5 || employeeId.isBlank()){
+            return ResponseEntity.badRequest().body("employee is invalid");
+
+        }
+
+        Person person = personController.getPersonByEmployeeId(employeeId);
+        if(person == null){
+            return ResponseEntity.notFound().build();
+
+        }
+        return ResponseEntity.ok(person);
+
+    }
+    @PostMapping("/createPerson")
+    public ResponseEntity<String>create(@Valid @RequestBody Person person){
+
+        personController.savePerson(person);
+        return new ResponseEntity<>("person created successfully", HttpStatus.OK);
+
+    }
+
 }
