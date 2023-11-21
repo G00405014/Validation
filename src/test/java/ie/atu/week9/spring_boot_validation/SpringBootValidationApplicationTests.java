821a506acc69767e1c8770f152c74f4132bad9c1
G00405014@atu.ie <G00405014@atu.ie>
2023-11-21 15:23:23 +0000

    Commit Dependencies

---
+++ src/test/java/ie/atu/week9/spring_boot_validation/SpringBootValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week9.spring_boot_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
