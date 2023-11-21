a199513fbaae795074192f748e74aedb0ce899a1
G00405014@atu.ie <G00405014@atu.ie>
2023-11-21 15:31:35 +0000

    Created Person Class

---
+++ src/main/java/Person.java
@@ -0,0 +1,7 @@
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+public class Person {
+}
